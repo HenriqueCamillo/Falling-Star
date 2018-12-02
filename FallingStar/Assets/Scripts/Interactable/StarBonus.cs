3a90c5484bd696d04188524f72bbcd9a380d8bc9
Unknown <will.leidan@gmail.com>
2018-12-02 18:05:50 -0200

    Basic of Bonus Stars and Reset

---
+++ FallingStar/Assets/Scripts/Interactable/StarBonus.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StarBonus : Interactable {
+
+    Color erasedColor;
+    public Image emblem;
+
+    bool got = false;
+
+    SpriteRenderer spr;
+    // Use this for initialization
+    void Start () {
+        erasedColor = emblem.color;
+        spr = GetComponent<SpriteRenderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public override void effect(GameObject player){
+        if (!got){
+            StageManager.instance.addStar();
+            emblem.color = Color.yellow;
+            got = true;
+            Invoke("DestroyStar", 0.05f);
+		}
+    }
+
+	void DestroyStar(){
+        spr.enabled = false;
+    }
+
+	public void Reset(){
+        spr.enabled = true;
+        got = false;
+        emblem.color = erasedColor;
+		
+    }
+}
---
+++ FallingStar/Assets/Scripts/StageManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StageManager : MonoBehaviour {
+
+    public static StageManager instance;
+
+    public StarBonus[] bonus;
+
+    //public Player player;
+    public GameObject player;
+    Vector3 playerStartPos;
+
+    public int stars = 0;
+
+    void Awake() {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(this.gameObject);
+
+        playerStartPos = player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+	public void Reset (){
+        player.transform.position = playerStartPos;
+        stars = 0;
+        foreach (StarBonus b in bonus)
+            b.Reset();
+        
+    }
+
+	public void addStar(){
+        stars++;
+    }
+}
