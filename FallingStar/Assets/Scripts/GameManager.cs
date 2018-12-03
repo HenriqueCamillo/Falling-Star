315f5375ba462667559f850ff32af6a04c910e09
Henrique Camillo <henriquecamillo@usp.br>
2018-12-03 21:50:28 -0200

    GameManager

---
+++ FallingStar/Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+	public static GameManager instance;
+	int[] stars;
+	int starTotal;
+	int currentLevel;
+
+	public AudioSource audioSource;
+	public AudioClip[] audioClip;
+
+
+	void Awake () {
+		if (instance == null) {
+			instance = this;
+		} else if (instance != this) {
+			Destroy(this);
+		}
+
+		audioSource = GetComponent<AudioSource>();
+		audioSource.clip = audioClip[0];
+		audioSource.Play();
+	}
+	
+	void Update() {
+		if (audioSource.isPlaying) {
+			Debug.Log("Playing");
+		}
+	}
+}
