3b74c87f0f8190e5b86900d682c69e27758cca9e
Unknown <will.leidan@gmail.com>
2018-12-03 22:07:13 -0200

    Recovery

---
+++ FallingStar/Assets/Scripts/Interactable/Recovery.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Recovery : Interactable {
+
+    public Particle[] particles;
+    Animator anim;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override void effect(GameObject player)
+    {
+        player.GetComponent<Star>().Shine = 1000;
+        transform.DetachChildren();
+        anim.SetTrigger("die");
+        foreach (Particle p in particles)
+            p.setTarget(player.transform);
+
+        Invoke("DestroyZone", 0.3f);
+    }
+
+    void DestroyZone()
+    {
+        Destroy(this.gameObject);
+    }
+}
